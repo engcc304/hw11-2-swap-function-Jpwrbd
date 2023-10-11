6b991af8fbf96665967072e60e126407201733af
Jpwrbd <jpwrbd456@gmail.com>
2023-10-11 23:32:13 +0700

    up

--- Homework/HW11/hw11-2-swap-function.cpp
+++ Homework/HW11/hw11-2-swap-function.cpp
@@ -22,4 +22,39 @@
         Name: Thane (31)
 
 */
+#include <stdio.h>
+#include <string>
+void age_user(int *age1,int *age2){ //ฟังก์ชั่นย่อย จะเรียกใช้ก็ต่อเมื่อประกาศใน main ฟังก์ชั่นหลัก
+    int temp = *age1;
+    *age1 = *age2;
+    *age2 = temp;
+}
+int main(){
+    int age1, age2 ;
+    char name1[10], name2[10] ;
+    printf( "Test case: \n" ) ;
+    printf( "\tInput A \n" ) ;
+    printf( "\tName : \n\t   " ) ;
+    scanf( "%s",&name1 ) ; 
 
+    printf( "\tAge : \n\t   " ) ;
+    scanf( "%d",&age2) ; 
+
+    printf( "\tInput B \n" ) ;
+    printf( "\tName : \n\t   " ) ;
+    scanf( "%s",&name2) ;    
+    printf("\tAge : \n\t   ") ;
+    scanf( "%d",&age2) ; 
+
+     //เรียกใช้ฟังก์ชั่นย่อย
+    printf( "Output: \n" ) ;
+    printf( "\t** RESULT ** \n" ) ;
+    printf ( "\tName: %s (%d)\n", name1 , age1 ) ;
+    printf ( "\tName: %s (%d)\n", name2 , age2 ) ;
+
+    printf ( "\n\t** SWAP AGE **\n" ) ;
+    printf ( "\tName: %s (%d)\n", name1 , age2 ) ;
+    printf ( "\tName: %s (%d)\n", name2 , age1 ) ;
+    
+    return 0 ;
+}
